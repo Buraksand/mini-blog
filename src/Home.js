2f0b6fab39a7d88992967d043144081e751e3777
Burak Sandiraz <buraksand@gmail.com>
2022-12-06 12:52:41 +0300

    fetching data is added

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState([]);
 
-  const handleDelete = (id) => {
-    console.log("deleted id", id);
-    const filteredBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(filteredBlogs);
-  };
+  useEffect(() => {
+    console.log("fetch");
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete} />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "mario")}
-        title={"Marios Blogs"}
-        handleDelete={handleDelete}
-      />
+      {blogs && <BlogList blogs={blogs} title={"All Blogs"} />}
     </div>
   );
 };
